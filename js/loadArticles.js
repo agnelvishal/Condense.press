5b9c9876f2ba017ae5cc42ebf0fac632a40f6a81
Vicki Clack <vicki.clack@intelliapps.co.uk>
2018-10-16 22:01:20 +0100

    Pass value if 'site' select from ajax call to articles.php, as presumably it will be used at some point.

--- js/loadArticles.js
+++ js/loadArticles.js
@@ -14,6 +14,7 @@ function loadArticles(event) {
         type: 'POST',
         url: "articles.php",
         data: {
+            'site': $('#articles-articles-site').val(),
             'fromDate': $('#articles-from-date').val(),
             'toDate': $('#articles-to-date').val()
         },
