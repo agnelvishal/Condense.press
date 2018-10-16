651a38407d0e534def964c99b38c73aa8c44654d
Vicki Clack <vicki.clack@intelliapps.co.uk>
2018-10-16 21:40:40 +0100

    Clean up merge conflict

--- js/autoSubmit.js
+++ js/autoSubmit.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
- document.forms["req"].submit();
-
-var inputs = document.getElementsByTagName("input");
-for (i=0; i<inputs.length; i++){
-   inputs[i].onchange = changeHandler;
-=======
 var inputs = document.getElementsByTagName('input');
 for (i = 0; i < inputs.length; i++) {
   inputs[i].onchange = changeHandler;
->>>>>>> b54e11780136fe3b02c06e6945bbe97278292c7b
 }
 document.getElementsByTagName('select')[0].onchange = function() {
   changeHandler();
