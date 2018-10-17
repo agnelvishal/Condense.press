125ee8e1add28e4bd73f40dc1e61e8d5c1690624
Vicki Clack <vicki.clack@intelliapps.co.uk>
2018-10-17 09:27:30 +0100

    Fix issues raised by Codacy/PR Quality Review, bar one which requires ECMA6.

--- js/loadArticles.js
+++ js/loadArticles.js
@@ -1,3 +1,25 @@
+function loadArticles(event) {
+    var request = $.ajax({
+        type: "POST",
+        url: "articles.php",
+        data: {
+            "site": $("#articles-articles-site").val(),
+            "fromDate": $("#articles-from-date").val(),
+            "toDate": $("#articles-to-date").val()
+        },
+        error: function (e) {
+            $("#articles-container").html("<p>Failed to load articles.</p>");
+        },
+        dataType: 'html'
+    });
+
+    $.when(request).done(function (html) {
+        $("#articles-container").html(html);
+    }).fail(function (response) {
+        $("#articles-container").html("<p>Failed to load articles.</p>");
+    });
+}
+
 $( document ).ready(function() {
     $('input').change(function () {
         loadArticles();
@@ -9,27 +31,3 @@ $( document ).ready(function() {
     loadArticles();
 });
 
-function loadArticles(event) {
-    var request = $.ajax({
-        type: 'POST',
-        url: "articles.php",
-        data: {
-            'site': $('#articles-articles-site').val(),
-            'fromDate': $('#articles-from-date').val(),
-            'toDate': $('#articles-to-date').val()
-        },
-        error: function (e) {
-            $("#articles-container").html("<p>Failed to load articles.</p>");
-            console.log(e);
-        },
-        dataType: 'html'
-    });
-
-    $.when(request).done(function (html) {
-        console.log(html);
-        $("#articles-container").html(html);
-    }).fail(function (response) {
-        $("#articles-container").html("<p>Failed to load articles.</p>");
-        console.log(response);
-    });
-}
