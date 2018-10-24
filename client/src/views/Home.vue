d1613fdedc4790560446004dd04936ec4ed7892c
skayablars <larizzatg@hotmail.com>
2018-10-23 21:17:00 -0400

    add css to app, and navbar

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <the-header />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import TheHeader from '@/components/TheHeader.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    TheHeader,
   },
 };
 </script>
