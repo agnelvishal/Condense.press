91cc1626e2367afb76796f9de470f3a76433b8a3
skayablars <larizzatg@hotmail.com>
2018-10-23 22:14:36 -0400

    Add feeds api

--- client/src/api/feeds.js
+++ client/src/api/feeds.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-// index
+// http://condense.press.test/rest-api/public/feeds
 const getFeeds = () => {
  
 };
 
+// http://condense.press.test/rest-api/public/feeds/between?startDate=2018-04-28&endDate=2018-04-29
 const getFeedsBetween = (startDate, endDate) => {
 
 };
